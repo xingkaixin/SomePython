86b8c17a3eaf6e8316ce1dae3f529d42be1a31b8
XingKaiXin <xingkaixin@gmail.com>
2015-08-27 02:46:56 +0800

    add gevent

--- Celery/job.py
+++ Celery/job.py
@@ -1,8 +1,24 @@
-from task import add
+from task import add,minus
 
 
 taskid =  add.delay(4,14)
-#taskid ='3e65d6b2-ddae-45ad-aaf4-8f1851171770'
+# taskid ='3e65d6b2-ddae-45ad-aaf4-8f1851171770'
 print taskid
-task = add.AsyncResult(taskid)
-print task.state,task.get()
+# task = add.AsyncResult(taskid)
+# print task.state,task.get()
+taskid2 = minus.delay(10,5)
+print taskid2
+
+# taskid ='99b0d2c5-7dae-4cf3-90ec-962b4db7c323'
+# task = add.AsyncResult(taskid)
+# print task.state
+
+
+for i in range(1,100):
+    taskid =  add.delay(4,14)
+    # taskid ='3e65d6b2-ddae-45ad-aaf4-8f1851171770'
+    print taskid
+    # task = add.AsyncResult(taskid)
+    # print task.state,task.get()
+    taskid2 = minus.delay(10,5)
+    print taskid2
--- Celery/task.py
+++ Celery/task.py
@@ -1,13 +1,27 @@
 from celery import Celery
+import time
 
 BROKER_URL = 'sqla+sqlite:///task.db'
 BACKEND_URL = 'db+sqlite:///task.db'
 
 app = Celery('tasks', broker=BROKER_URL, backend=BACKEND_URL)
 
+"""
+celery -A task worker -P gevent --loglevel=info
+"""
+
 
 @app.task
 def add(x, y):
-    import time
-    time.sleep(25)
+    time.sleep(1)
     return x + y
+
+
+@app.task
+def minus(x, y):
+    i = 0
+    while i < 50:
+        time.sleep(1)
+        print x - y
+        i += 10
+    return x - y
